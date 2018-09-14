4bd30b6d295e2446e474a8b6d9a6c612a59d6c44
Jesper Pedersen <jesper@yepz.dk>
2018-09-14 11:03:47 +0200

    NOR-62: Templates. Refactor JSON

--- app/routes.py
+++ app/routes.py
@@ -2,30 +2,31 @@ from flask import render_template
 from flask import request
 from app import app
 import json
+import requests
 
 @app.route('/')
 @app.route('/index')
 def index():
-    robot = {'name': 'Norma'}
     stories = [
         {   
-            'title': 'Bog 1',
-            'author': {'firstname': 'John', 'lastname': 'Jones'},
-            'cover_url': '//placehold.it/200x300&text=bog1',
-            'data_url': 'static/stories/drageridderen/drageridderen.json'
+            'id' : '1',
+            'data_url': 'static/stories/book1/book1.json'
         },
         {   
-            'title': 'Bog 2',
-            'author': {'firstname': 'John', 'lastname': 'Jones'},
-            'cover_url': '//placehold.it/200x300&text=bog2',
-            'data_url': 'static/stories/drageridderen/drageridderen.json'
+            'id' : '2',
+            'data_url': 'static/stories/book2/book2.json'
         },
     ]
-    return render_template('index.html', title='Robotten min laesemakker', robot=robot, stories=stories )
+    # with app.open_resource('static/stories/book1/book1.json') as f:
+    #    data = json.load(f)
+    return render_template('index.html', title='Robotten min laesemakker', stories=stories )
 
 @app.route('/story')
 def story():
-    return render_template('story.html', title='story')
+    story_id = request.args.get('story', None)
+    #response = requests.get("static/stories/drageridderen/drageridderen.json")
+    #todos = json.loads(response.text)
+    return render_template('story.html', title='story', story_id=story_id )
 
 @app.route('/page')
 def page():

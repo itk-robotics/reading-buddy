b3bcba70dcedf9034bcd2fec6e0c97322157f2ef
Anders Krogsager <andkr@aarhus.dk>
2018-09-14 11:29:05 +0200

    commit after pull

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,8 @@ from flask import render_template
 from flask import request
 from app import app
 import json
-import requests
 
-@app.route('/')
-@app.route('/index')
-def index():
-    stories = [
+stories = [
         {   
             'id' : '1',
             'data_url': 'static/stories/book1/book1.json'
@@ -17,13 +13,27 @@ def index():
             'data_url': 'static/stories/book2/book2.json'
         },
     ]
+<<<<<<< Updated upstream
     with app.open_resource('static/stories/book1/book1.json') as f:
         data = json.load(f)
     return render_template('index.html', title='Robotten min laesemakker', stories=stories, data=data)
+=======
+
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    return render_template('index.html', title='Robotten min laesemakker', stories=stories )
+>>>>>>> Stashed changes
 
 @app.route('/story')
 def story():
     story_id = request.args.get('story', None)
+<<<<<<< Updated upstream
+=======
+    print story_id
+    
+>>>>>>> Stashed changes
     return render_template('story.html', title='story', story_id=story_id )
 
 @app.route('/page')
@@ -36,4 +46,9 @@ def choice():
 
 @app.route('/question')
 def question():
-    return render_template('question.html', title='question')    
+    return render_template('question.html', title='question')
+
+
+
+#def story_parse()
+#    print stories[0].keys()[story_id][1]

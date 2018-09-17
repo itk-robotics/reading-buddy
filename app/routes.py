1298ab6775fda46274896944224926de493d8f11
Anders Krogsager <andkr@aarhus.dk>
2018-09-17 16:53:17 +0200

    increment

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,10 @@ story_data = []
 story_id = -1
 my_story = None
 json_path = ""
+chapter = -1
+page = -1
+animation = ["type","synchronicity", "path"] #e.g. ["say", 'async', "animations/Stand/Sky_01"]
+active_story = "" #dict from json
 
 @app.route('/')
 @app.route('/index')
@@ -25,32 +29,92 @@ def index():
 
 @app.route('/story')
 def story():
+    global story_data, story_id, my_story, json_path, chapter, page, animation
+
     story_id = int(request.args.get('story', None))
     json_path = stories[story_id]
     fprint(json_path)
-    json_path = json_path.rsplit('/',1)[0] #cut off *.json in path
-    return render_template('story.html', title='story', story_data=my_story, story_id=story_id, story_path=json_path)
+    json_path.rsplit('/',1)[0] #cut off *.json in path.
+    init_story(json_path)
+    return render_template('story.html', title='story', story_data=my_story, story_id=story_id, story_path=json_path.rsplit('/',1)[0])
 
 @app.route('/page')
 def page():
+    global story_data, story_id, my_story, json_path, chapter, page, animation, active_story
+
     #from json: chapters -> pages -> content
     #check for animations.
+    page_content = story_tracker()
+    fprint("page content:")
+    fprint(page_content)
     
-    return render_template('page.html', title='page', content = )
+    return render_template('page.html', title='page', content = page_content)
 
 
 @app.route('/choice')
 def choice():
+    global story_data, story_id, my_story, json_path, chapter, page, animation, active_story
+
     return render_template('choice.html', title='choice')    
 
 @app.route('/question')
 def question():
+    global story_data, story_id, my_story, json_path, chapter, page, animation, active_story
+
     return render_template('question.html', title='question')    
+
+
+
+""""*** utilities ***"""
 
 
 def read_json(path):
     with app.open_resource(path) as f:
         return json.load(f)
-            
+
 def fprint(data):
     print(data, file=sys.stdout)
+
+def init_story(path):
+    global story_data, story_id, my_story, json_path, chapter, page, animation, active_story
+
+    active_story = read_json(path)
+    chapter = len(active_story['chapters'])
+    fprint("story name: " + active_story['title']+". Story author: " + active_story['author'])
+    fprint("number of chapters: " + str(chapter))
+
+def story_tracker():
+    global story_data, story_id, my_story, json_path, chapter, page, animation, active_story
+
+    if len(active_story['chapters']) > 0:
+        #there are unread chapters
+        fprint("chapter count: " + str(len(active_story['chapters'])))
+
+        if len(active_story['chapters'][0]['pages']) > 0:
+            #there are unread pages
+            fprint("page count: " + str(len(active_story['chapters'][0]['pages'])))
+            #check if animation is given
+            page = active_story['chapters'][0]['pages'][0] #get uppermost page
+
+            if 'animation' in page.keys():
+                fprint("do say()/runBehavior(): " + page['animation'])
+
+            #data to return
+            page_content = active_story['chapters'][0]['pages'][0]['content']
+
+            #finally
+            active_story['chapters'][0]['pages'].pop(0)
+
+        else:
+            #end of chapter
+            question = active_story['chapters'][0]['question']
+            fprint(question)
+            active_story['chapters'].pop(0)
+            return ["question", question]
+    else:
+        #all chapters have been read
+        fprint("story is complete!")
+        return ["complete", ""]
+
+    return ["content", page_content]
+

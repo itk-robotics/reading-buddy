359b14b2156cab69d18830c1f2de9c523be5ead5
Unknown <andkr@aarhus.dk>
2019-01-10 16:07:05 +0100

    add text to nedslag 1 and 2

--- translations/translation_da_DK.ts
+++ translations/translation_da_DK.ts
@@ -1,3 +1,149 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="da_DK"/>
+<TS version="2.1" language="da_DK">
+    <context>
+        <name>animations/Bange/behavior.xar:/Say</name>
+        <message>
+            <location filename="animations/Bange/behavior.xar" line="0"/>
+            <source>Det kender jeg godt. Jeg bliver også nogle gange bange når jeg står alene på biblioteket om natten. \pau=500\ Her er mange mærkelige lydde. \pau=500\ Jeg ville også være rigtig bange hvis der var en drage som Blege på biblioteket. Den virker stor og farlig. Nu forstår jeg historien meget bedre. Nu skal vi læse næste kapitel.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det kender jeg godt. Jeg bliver også nogle gange bange når jeg står alene på biblioteket om natten. \pau=500\ Her er mange mærkelige lydde. \pau=500\ Jeg ville også være rigtig bange hvis der var en drage som Blege på biblioteket. Den virker stor og farlig. Nu forstår jeg historien meget bedre. Nu skal vi læse næste kapitel.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Bekymret/behavior.xar:/Say</name>
+        <message>
+            <location filename="animations/Bekymret/behavior.xar" line="0"/>
+            <source>Det kender jeg godt. Jeg bliver også nogle gange bekymret for at ingen vil snakke med mig mere. \pau=400\ Jeg kan også godt forstå at Tam er bekymret for dragen, for den ser ud til at være såret eller syg. Nu forstår jeg historien meget bedre. Nu skal vi læse næste kapitel.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det kender jeg godt. Jeg bliver også nogle gange bekymret for at ingen vil snakke med mig mere. \pau=400\ Jeg kan også godt forstå at Tam er bekymret for dragen, for den ser ud til at være såret eller syg. Nu forstår jeg historien meget bedre. Nu skal vi læse næste kapitel.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Familie/behavior.xar:/Say</name>
+        <message>
+            <location filename="animations/Familie/behavior.xar" line="0"/>
+            <source>Jeg forstår godt at familien fylder meget i Tams tanker, for han har jo ikke nogen. Jeg ved heller ikke hvem miene forældree er. \pau=1500\ Til gengæld har jeg rigtig mange venner</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jeg forstår godt at familien fylder meget i Tams tanker, for han har jo ikke nogen. Jeg ved heller ikke hvem miene forældree er. \pau=1500\ Til gengæld har jeg rigtig mange venner</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Intro/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="animations/Intro/behavior.xar" line="0"/>
+            <source>Historien I skal i gang med at læse for mig hedder Drageridderne \pau=300\ - Tiggerdrengen Tam
+\pau=1000\ Den handler om drengen Tam, der bor på gaderne i byen Demaar \pau=400\i nærheden af Dar-floden. \pau=1000\ Hans venner er drengen Indre, soldaten Hind, og dragerne Blege, Røde, Lette, Bella, Sorte, Skygge, smaa raud og Sky.
+\pau=1000\ Jeg glæder mig til at høre historien. I kan nu gå i gang med at læse for mig.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Historien I skal i gang med at læse for mig hedder Drageridderne \pau=300\ - Tiggerdrengen Tam
+\pau=1000\ Den handler om drengen Tam, der bor på gaderne i byen Demaar \pau=400\i nærheden af Dar-floden. \pau=1000\ Hans venner er drengen Indre, soldaten Hind, og dragerne Blege, Røde, Lette, Bella, Sorte, Skygge, smaa raud og Sky.
+\pau=1000\ Jeg glæder mig til at høre historien. I kan nu gå i gang med at læse for mig.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Nedslag1/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="animations/Nedslag1/behavior.xar" line="0"/>
+            <source>Det var vel nok dejligt at få læst højt. Historien er meget spændende. Jeg elsker historier om drager. Men jeg har brug for jeres hjælp til at forstå hvad Tam tænker. Tror I Tam er mest bange eller bekymret?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det var vel nok dejligt at få læst højt. Historien er meget spændende. Jeg elsker historier om drager. Men jeg har brug for jeres hjælp til at forstå hvad Tam tænker. Tror I Tam er mest bange eller bekymret?</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Nedslag_2/behavior.xar:/Animated Say</name>
+        <message>
+            <location filename="animations/Nedslag_2/behavior.xar" line="0"/>
+            <source>Det var et rigtigt godt kapitel. Jeg fik meget mere at vide om Tam og forstår ham lidt bedre nu. \pau=500\ Nu ved jeg lidt mere om Tam og dem han kender. Hvem tror I er vigtigst for Tam. Er det Venner eller Familie?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det var et rigtigt godt kapitel. Jeg fik meget mere at vide om Tam og forstår ham lidt bedre nu. \pau=500\ Nu ved jeg lidt mere om Tam og dem han kender. Hvem tror I er vigtigst for Tam. Er det Venner eller Familie?</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/Venner/behavior.xar:/Say</name>
+        <message>
+            <location filename="animations/Venner/behavior.xar" line="0"/>
+            <source>Jeg forstår godt at vennerne betyder rigtig meget for Tam. \pau=1200\ Jeg har heller ingen familie, men jeg har heldigvis mange venner.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jeg forstår godt at vennerne betyder rigtig meget for Tam. \pau=1200\ Jeg har heller ingen familie, men jeg har heldigvis mange venner.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Bange/Say</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Det kender jeg godt. Jeg bliver også nogle gange bange når jeg står alene på biblioteket om natten. \pau=500\ Her er mange mærkelige lydde. \pau=500\ Jeg ville også være rigtig bange hvis der var en drage som Blege på biblioteket. Den virker stor og farlig.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det kender jeg godt. Jeg bliver også nogle gange bange når jeg står alene på biblioteket om natten. \pau=500\ Her er mange mærkelige lydde. \pau=500\ Jeg ville også være rigtig bange hvis der var en drage som Blege på biblioteket. Den virker stor og farlig.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Bekymret/Say</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Det kender jeg godt. Jeg bliver også nogle gange bekymret for at ingen vil snakke med mig mere. \pau=400\ Jeg kan også godt forstå at Tam er bekymret for dragen, for den ser ud til at være såret eller syg.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det kender jeg godt. Jeg bliver også nogle gange bekymret for at ingen vil snakke med mig mere. \pau=400\ Jeg kan også godt forstå at Tam er bekymret for dragen, for den ser ud til at være såret eller syg.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/EfterNedslag1</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Tak, nu forstår jeg historien meget bedre. Skal vi ikke læse næste kapitel nu?\pau=500\ Er gruppe 2 parat til at læse højt for mig. Jeg lytter nu.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tak, nu forstår jeg historien meget bedre. Skal vi ikke læse næste kapitel nu?\pau=500\ Er gruppe 2 parat til at læse højt for mig. Jeg lytter nu.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Familie/Say</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Jeg forstår godt at familien fylder meget i Tams tanker, for han har jo ikke nogen. Jeg ved heller ikke hvem miene forældree er. \pau=1500\ Til gengæld har jeg rigtig mange venner</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jeg forstår godt at familien fylder meget i Tams tanker, for han har jo ikke nogen. Jeg ved heller ikke hvem miene forældree er. \pau=1500\ Til gengæld har jeg rigtig mange venner</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Intro</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Historien I skal i gang med at læse for mig hedder Drageridderne \pau=300\ - Tiggerdrengen Tam
+\pau=1000\ Den handler om drengen Tam, der bor på gaderne i byen Demaar \pau=400\i nærheden af Dar-floden. \pau=1000\ Hans venner er drengen Indre, soldaten Hind, og dragerne Blege, Røde, Lette, Bella, Sorte, Skygge, smaa raud og Sky.
+\pau=1000\ Jeg glæder mig til at høre historien. I kan nu gå i gang med at læse for mig.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Historien I skal i gang med at læse for mig hedder Drageridderne \pau=300\ - Tiggerdrengen Tam
+\pau=1000\ Den handler om drengen Tam, der bor på gaderne i byen Demaar \pau=400\i nærheden af Dar-floden. \pau=1000\ Hans venner er drengen Indre, soldaten Hind, og dragerne Blege, Røde, Lette, Bella, Sorte, Skygge, smaa raud og Sky.
+\pau=1000\ Jeg glæder mig til at høre historien. I kan nu gå i gang med at læse for mig.</translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Nedslag 1</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Det var vel nok dejligt at få læst højt. Historien er meget spændende. Jeg elsker historier om drager. Men jeg har brug for jeres hjælp til at forstå hvad Tam tænker. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det var vel nok dejligt at få læst højt. Historien er meget spændende. Jeg elsker historier om drager. Men jeg har brug for jeres hjælp til at forstå hvad Tam tænker. </translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Nedslag 2</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Det var et rigtigt godt kapitel. Jeg fik meget mere at vide om Tam og forstår ham lidt bedre nu. \pau=500\ Men hvad tror I fylder mest i Tams tanker? Familien eller vennerne?  </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Det var et rigtigt godt kapitel. Jeg fik meget mere at vide om Tam og forstår ham lidt bedre nu. \pau=500\ Men hvad tror I fylder mest i Tams tanker? Familien eller vennerne?  </translation>
+        </message>
+    </context>
+    <context>
+        <name>animations/WizardOfOz/behavior.xar:/Venner/Say</name>
+        <message>
+            <location filename="animations/WizardOfOz/behavior.xar" line="0"/>
+            <source>Jeg forstår godt at vennerne betyder rigtig meget for Tam. \pau=1200\ Jeg har heller ingen familie, men jeg har heldigvis mange venner. \pau=800\ Selvom jeg har mange venner, så kan jeg altid bruge flere. \pau=400\ Og jeg vil gerne have jer – mine nye læsevenner op på væggen også.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Jeg forstår godt at vennerne betyder rigtig meget for Tam. \pau=1200\ Jeg har heller ingen familie, men jeg har heldigvis mange venner. \pau=800\ Selvom jeg har mange venner, så kan jeg altid bruge flere. \pau=400\ Og jeg vil gerne have jer – mine nye læsevenner op på væggen også.</translation>
+        </message>
+    </context>
+</TS>
